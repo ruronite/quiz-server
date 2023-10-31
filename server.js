28ac76b01c1d6f3642bd092fd1ced9cc80a15cf8
ogheneruro <ruronation2@yahoo.com>
2023-10-31 10:25:42 +0100

    added cors origin

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "https://worldquizglobal.netlify.app/"
+}))
 
 /*
 app.get("/bulkinsert", async (req, res, next) => {
